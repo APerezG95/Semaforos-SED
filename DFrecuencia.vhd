3c9fd3a63ab29fbc09c8be0e10947628e738d12e
Alvaro Zornoza Uña <azuzornoz@gmail.com>
2016-11-22 16:18:05 +0100

    Creación del archivo DFrecuencia.vhd

---
+++ DFrecuencia.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- University:    Universidad Politécnica de Madrid
+-- School			Escuela Técnica Superior de Ingeneria y Diseño Industrial
+-- Subject			Sistemas Infomáticos Digitales
+-- Engineer: 		Adrían Pérez
+--						Jorge Scharfhausen
+--						Alvaro Zornoza
+-- 
+-- Create Date:    16:02:34 11/22/2016 
+-- Design Name:    Trabajo de la asignatura.
+-- Module Name:    DFrecuencia - Behavioral 
+-- Project Name:   Cruce de semáforos
+-- Target Devices: Xilinx Spartan 3
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DFrecuencia is
+end DFrecuencia;
+
+architecture Behavioral of DFrecuencia is
+
+begin
+
+
+end Behavioral;
+
