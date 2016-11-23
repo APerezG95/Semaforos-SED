519edf60b374cd2802098760b5c42a9d59126561
Alvaro Zornoza Uña <azuzornoz@gmail.com>
2016-11-23 15:30:13 +0100

    Primeras líneas de código del trabajo
    -Incorporación del divisor de frecuencia
    -Incorporación de los estados y sus salidas
    -Creación de la entidad PSemaforos que gestionará el parpadeo de los
    semáforos.

--- MEstados.vhd
+++ MEstados.vhd
@@ -2,7 +2,7 @@
 -- University:    Universidad Politécnica de Madrid
 -- School			Escuela Técnica Superior de Ingeneria y Diseño Industrial
 -- Subject			Sistemas Infomáticos Digitales
--- Engineer: 		Adrían Pérez
+-- Engineers: 		Adrían Pérez
 --						Jorge Scharfhausen
 --						Alvaro Zornoza
 -- 
@@ -12,7 +12,7 @@
 -- Project Name:   Cruce de semáforos
 -- Target Devices: Xilinx Spartan 3
 -- Tool versions: 
--- Description: 
+-- Description: Esta entidad gestiona la maquina de estados y sus transiciones
 --
 -- Dependencies: 
 --
@@ -34,12 +34,272 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity MEstados is
+    Port ( 	
+			  clk : in  STD_LOGIC; -- Reloj
+           rst : in  STD_LOGIC; -- Reset asíncrono
+			  pulsadorPP : in  STD_LOGIC; -- Pulsador del semáforo de peatones principal
+			  pulsadorPS : in  STD_LOGIC; -- Pulsador del semáforo de peatones secundario
+			  sensorCS: in STD_LOGIC; -- Sensor de vehículos en carretera secundaria
+			  sensorTR: in STD_LOGIC; -- Sensor que detecta la presencia de un tren. Funciona por nivel. Mientras está a 1, hay tren en la via
+           SPV : out  STD_LOGIC; -- Semáforo principal en verde
+           SPR : out  STD_LOGIC; -- Semáforo principal en rojo
+           SPN : out  STD_LOGIC; -- Semáforo principal en naranja
+           PPV : out  STD_LOGIC; -- Semáforo de peatones principal en verde
+			  PPVP: out	 STD_LOGIC; -- Entrada de habilitación para de la entidad parpadeo de semáforo peatones principal
+           PPR : out  STD_LOGIC; -- Semáforo de peatones principal en rojo
+           SSV : out  STD_LOGIC; -- Semáforo secundario en verde
+           SSR : out  STD_LOGIC; -- Semáforo secundario en rojo
+           SSN : out  STD_LOGIC; -- Semáforo secundario en naranja
+           PSV : out  STD_LOGIC; -- Semáforo de peatones secundario en verde
+			  PSVP:  out	 STD_LOGIC; -- Entrada de habilitación para de la entidad parpadeo de semáforo peatones secundario
+           PSR : out  STD_LOGIC; -- Semáforo de peatones secundario en rojo
+           trainIN : out  STD_LOGIC; -- Led que indica la presencia de un tren en la via y provoca el cierre de los dos semásforos de los coches
+           trainOUT : out  STD_LOGIC -- Led que indica que el tren se ha ido y se inicia el estado de transición hacia estado S0
+			); 
 end MEstados;
+
 
 architecture Behavioral of MEstados is
 
-begin
+	TYPE STATES is (s0, s1, s2, s3, t1, t2);
 
+	SIGNAL current_state: STATES;
+	SIGNAL next_state: STATES;
+	
+	signal cambio_estado: std_logic;	-- valor '1' cuando se ha llegado al tiempo requerido
+	
+	
+	
+	constant tmax: integer :=15;			--tiempo máximo que van a estar los semaforos
+	constant tambar:integer:=4;			--tiempo máximo que van a estar los semaforos en ambar
+	constant tespera:integer:=4;			--tiempo máximo a esperar después de pulsar el botón para que pase a ambar
+	
+	signal tiempo:integer range 0 to tmax;
+	
+	signal cnt: integer range 0 to tmax; --contador 
+	
+	begin
+	
+
+----------- REGISTRO DE ESTADOS ---------------
+
+	registro_de_estados: process (rst,clk)
+	
+			begin
+			
+			if rst = '1' then 
+				current_state <= s0;
+				 
+				 
+			elsif rising_edge(clk) then
+			
+					if cambio_estado='1' then
+						current_state<=next_state;			-- se cambia de estado, se reinicia la cuenta y se pone a cero la bandera
+												
+					end if;
+				end if;						
+	end process;
+	
+	
+----------------------CONTADOR------------------
+	
+	contador :process (clk)
+		
+		begin
+						
+			if rising_edge(clk) then
+			
+					if (cnt<tiempo) then
+						cnt<=cnt+1;
+						cambio_estado<='0';
+						
+					elsif cnt=tiempo then
+						cambio_estado<='1';
+						cnt<=0;
+						
+					end if;
+				
+			end if;
+		end process;
+		
+		
+		
+------------------ PRÓXIMO ESTADO -----------------------------
+
+		
+	proximo_estado: process (current_state, pulsadorPP, pulsadorPS)--la sentencia WAIT solo se puede usar en un proceso SIN LISTA DE SENSIBILIDAD
+	
+		begin 
+
+			next_state<=current_state;
+
+			case current_state is 
+
+				when s0 =>
+						
+						if pulsadorPP = '1' then  
+								tiempo<=	tespera;-- Si se da al pulsador, esperamos el tiempo de espera, 
+								next_state<=s1;				--	y en el siguiente ciclo de reloj se cambiaría de estado al estar activada la bandera de cambio de estado
+								
+						else
+											-- Si no se ha dado a ningún pulsador, cuando la cuenta llegue al máximo se cambia de estado
+								tiempo<=tmax;
+								next_state <= s1;
+								
+							
+						end if;
+						
+				when s1 =>
+					
+							tiempo<=tambar;
+							next_state <= s2;
+							
+				when s2 =>
+						
+						if (pulsadorPP = '1' or pulsadorPS = '1') then
+								tiempo<=tespera; 
+								next_state<=s3;		
+																
+						else
+								tiempo<=tmax; 
+								next_state <= s3;
+							
+						end if;
+				
+				when s3 =>
+							
+							tiempo<=tambar;
+							next_state <= s0;
+					
+						
+				when others => next_state <= s0;
+
+		end case;
+
+	end process;
+
+
+
+----------------SALIDAS----------------------
+
+	salidas: process(current_state)
+	
+		begin
+
+		case current_state is 
+
+			when s0 =>
+					SPV      <= '1'
+					SPR      <= '0'
+					SPN      <= '0'
+					PPV      <= '0'
+					PPVP     <= '0'
+					PPR      <= '1'
+					SSV      <= '0'
+					SSR      <= '1'
+					SSN      <= '0'
+					PSV      <= '1'
+					PSVP     <= '0'
+					PSR      <= '0'
+					trainIN  <= '0'
+				   trainOUT <= '0'
+					
+			when s1 =>
+					SPV      <= '0'
+					SPR      <= '0'
+					SPN      <= '1'
+					PPV      <= '0'
+					PPVP     <= '0'
+					PPR      <= '0'
+					SSV      <= '0'
+					SSR      <= '1'
+					SSN      <= '0'
+					PSV      <= '1'
+					PSVP     <= '1'
+					PSR      <= '0'
+					trainIN  <= '0'
+				   trainOUT <= '0'
+
+			when s2 =>
+					SPV      <= '0'
+					SPR      <= '1'
+					SPN      <= '0'
+					PPV      <=	'1'
+					PPVP     <= '0'
+					PPR      <= '0'
+					SSV      <= '1'
+					SSR      <=	'0'
+					SSN      <=	'0'
+					PSV      <= '0'
+					PSVP     <= '0'
+					PSR      <= '1'
+					trainIN  <= '0'
+				   trainOUT <= '0'
+			
+			when s3 =>
+					SPV      <= '0'
+					SPR      <= '1'
+					SPN      <= '0'
+					PPV      <=	'1'
+					PPVP     <= '1'
+					PPR      <= '0'
+					SSV      <= '0'
+					SSR      <=	'0'
+					SSN      <=	'1'
+					PSV      <= '0'
+					PSVP     <= '0'
+					PSR      <= '1'
+					trainIN  <= '0'
+				   trainOUT <= '0'
+					
+			when t1 => 
+					SPV      <= '0';
+					SPR      <= '1';
+					SPN      <= '0';
+					PPV      <= '1';
+					PPVP     <= '0';
+					PPR      <= '0';
+					SSV      <= '0';
+					SSR      <= '1';
+					SSN      <= '0';
+					PSV      <= '1';
+					PSVP     <= '0';
+					PSR      <= '0';
+					trainIN  <= '1';
+				   trainOUT <=	'0';	
+					
+			when t2 => 
+					SPV      <= '0';
+					SPR      <= '1';
+					SPN      <= '0';
+					PPV      <= '1';
+					PPVP     <= '1';
+					PPR      <= '0';
+					SSV      <= '0';
+					SSR      <= '1';
+					SSN      <= '0';
+					PSV      <= '1';
+					PSVP     <= '0';
+					PSR      <= '0';
+					trainIN  <= '0';
+				   trainOUT <=	'1';	
+					
+			when others => 
+					SPV      <= '0';
+					SPN      <= '0';
+					PPV      <= '0';
+					PPVP     <= '0';
+					PPR      <= '0';
+					SSV      <= '0';
+					SSR      <= '0';
+					SSN      <= '0';
+					PSV      <= '0';
+					PSVP     <= '0';
+					PSR      <= '0';
+					trainIN  <= '0';
+				   trainOUT <=	'0';	
+		end case;
+
+	end process;
 
 end Behavioral;
-
--- PSemaforos.vhd
+++ PSemaforos.vhd
@@ -8,17 +8,18 @@
 -- 
 -- Create Date:    16:02:34 11/22/2016 
 -- Design Name:    Trabajo de la asignatura.
--- Module Name:    DFrecuencia - Behavioral 
+-- Module Name:    PSemaforos - Behavioral 
 -- Project Name:   Cruce de semáforos
 -- Target Devices: Xilinx Spartan 3
 -- Tool versions: 
--- Description: Esta entidad toma la señal de 50Mhz de la FPGA y divide la frecuencia a 1HZ
+-- Description: Esta entidad sen encarga de gestionar el parpadeo de los semaforos de peatones.
 --
 -- Dependencies: 
 --
 -- Revision: 
 -- Revision 0.01 - File Created
 -- Additional Comments: 
+--
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -32,32 +33,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity DFrecuencia is
-    Port ( entrada : in  STD_LOGIC;
-           reset : in  STD_LOGIC;
-           salida : out  STD_LOGIC);
-end DFrecuencia;
+entity PSemaforos is
+end PSemaforos;
 
+architecture Behavioral of PSemaforos is
 
-architecture Behavioral of DFrecuencia is
-	signal temporal: STD_LOGIC;
-   signal contador: integer range 0 to 24999999 := 0; -- De 50 MHz a 1 Hz
 begin
-divisor_frecuencia: process (reset, entrada) 
-	begin
-        if (reset = '1') then
-            temporal <= '0';
-            contador <= 0;
-        elsif rising_edge(entrada) then
-            if (contador = 24999999) then
-                temporal <= NOT(temporal);
-                contador <= 0;
-            else
-                contador <= contador+1;
-            end if;
-        end if;
-    end process; 
-	 salida <= temporal;
-end Behavioral;
 
+
+end Behavioral;
 
