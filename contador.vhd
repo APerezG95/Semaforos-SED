25140f22cc62065682bbfefa0f5574b876012fb1
Scharfhausen <j.sromero@alumnos.upm.es>
2017-01-05 10:12:30 +0100

    Revert "Corregido el problema del sintetizado"
    This reverts commit 546655498e498b824ab9fd11ba315729aab0cd62.

--- contador.vhd
+++ contador.vhd
@@ -40,25 +40,27 @@ end contador;
 
 architecture Behavioral of contador is
 
-constant tmax	:integer:=120;       							--constante auxiliar para poder asignar señales temporales
-signal cnt		:integer range 0 to tmax:=0; 					--contador 
-shared variable aux :integer range 0 to 1 := 0;				--variable interna
---signal aux_process: integer range 0 to 1 := 0;				
+constant tmax	:integer:=120;       --constante auxiliar para poder asignar señales temporales
+signal cnt		:integer range 0 to tmax:=0; --contador 
+shared variable aux :integer range 0 to 1:=0;--variable interna
  
 begin 
 
-process (fastclk,tiempo,clk)
-	
-	variable alltiempo: integer range 0 to tmax:=0;
+process (fastclk,tiempo)
+	variable alltiempo: integer range 0 to 120:=0;
 	begin
-		if fastclk='1' then		
+		if rising_edge(fastclk) then		
 			if alltiempo/=tiempo then
 				aux:=aux+1;
 			end if;
 			alltiempo:=tiempo;
 		end if;
-		if clk='0' then
-		if aux>0 then 
+end process;
+	
+process (clk,tiempo)
+	begin
+		if falling_edge(clk) then
+			if aux>0 then 
 				cnt<=0;
 				aux:=0;
 			else
@@ -66,18 +68,6 @@ process (fastclk,tiempo,clk)
 			end if;
 		end if;
 end process;
-	
---process (clk,tiempo)
---	begin
---		if falling_edge(clk) then
---			if aux>0 then 
---				cnt<=0;
---				aux:=0;
---			else
---				cnt<=cnt+1;									
---			end if;
---		end if;
---end process;
 	
 process(cnt,tiempo)
 		begin
